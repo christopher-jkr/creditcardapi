633bdc60e1c2d466c43c7a627a2cbb24a45de5b9
James Uanhoro <stonegold546@gmail.com>
2015-05-01 09:34:12 +0800

    Reducing rubocop errors

--- lib/luhn_validator.rb
+++ lib/luhn_validator.rb
@@ -5,12 +5,16 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
 
+  def doubler(x)
+    2 * x >= 10 ? 2 * x - 9 : 2 * x
+  end
+
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     nums_a.reverse!
     checksum = 0
     nums_a.each_with_index do |x, idx|
-      2 * x >= 10 ? b = 2 * x - 9 : b = 2 * x
+      b = doubler(x)
       idx.odd? ? checksum += b : checksum += x
     end
     checksum % 10 == 0
