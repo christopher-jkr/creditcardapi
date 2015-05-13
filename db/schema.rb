00c2f1649eec05bfbf9728c47547f40bb82081fd
James Uanhoro <stonegold546@gmail.com>
2015-05-13 15:50:20 +0800

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422114618) do
+ActiveRecord::Schema.define(version: 20150513073821) do
 
   create_table "credit_cards", force: :cascade do |t|
     t.string "expiration_date"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20150422114618) do
     t.string "credit_network"
     t.string "nonce_64"
     t.text   "encrypted_number"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "fullname"
+    t.date     "dob"
+    t.text     "hashed_password"
+    t.text     "address"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
