efa4e688e80c249eec9b6cef9b23dbb5c2257282
James Uanhoro <stonegold546@gmail.com>
2015-05-21 22:41:03 +0800

    Shifted things around, purely aesthetic

--- helpers/creditcardapi_helper.rb
+++ helpers/creditcardapi_helper.rb
@@ -5,21 +5,6 @@ require 'pony'
 
 # Helper module for CreditCardAPI class
 module CreditCardHelper
-  def login_user(user)
-    payload = { user_id: user.id }
-    token = JWT.encode payload, ENV['MSG_KEY'], 'HS256'
-    session[:auth_token] = token
-    redirect '/'
-  end
-
-  def find_user_by_token(token)
-    return nil unless token
-    decoded_token = JWT.decode token, ENV['MSG_KEY'], true
-    payload = decoded_token.first
-    logger.info "PAYLOAD: #{payload}"
-    User.find_by_id(payload['user_id'])
-  end
-
   # Handling user registration
   class Registration
     attr_accessor :username, :password, :email, :dob, :address, :fullname
@@ -36,6 +21,21 @@ module CreditCardHelper
         var && var.length > 0
       end
     end
+  end
+
+  def login_user(user)
+    payload = { user_id: user.id }
+    token = JWT.encode payload, ENV['MSG_KEY'], 'HS256'
+    session[:auth_token] = token
+    redirect '/'
+  end
+
+  def find_user_by_token(token)
+    return nil unless token
+    decoded_token = JWT.decode token, ENV['MSG_KEY'], true
+    payload = decoded_token.first
+    logger.info "PAYLOAD: #{payload}"
+    User.find_by_id(payload['user_id'])
   end
 
   def email_registration_verification(registration)
