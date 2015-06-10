12e836446fd526488f893c401070202903bb2be1
James Uanhoro <stonegold546@gmail.com>
2015-06-10 16:06:32 +0800

    Added user_id to local dbs

--- db/migrate/20150422114618_create_credit_cards.rb
+++ db/migrate/20150422114618_create_credit_cards.rb
@@ -2,7 +2,7 @@
 class CreateCreditCards < ActiveRecord::Migration
   def change
     create_table :credit_cards do |cc|
-      cc.string :expiration_date, :owner, :credit_network, :nonce_64
+      cc.string :expiration_date, :owner, :credit_network, :nonce_64, :user_id
       cc.text :encrypted_number
       cc.timestamps null: false
     end
